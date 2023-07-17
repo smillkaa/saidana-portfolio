dafe5854ce02bacd7aeb87af70001a2eb628db69
saidananarmo <104727784+smillkaa@users.noreply.github.com>
2023-07-17 15:21:18 +0300

    done

--- assets/js/copyright.js
+++ assets/js/copyright.js
@@ -3,7 +3,7 @@ const copyright = document.getElementById('copyright')
 
 function displayCopyright() {
 
-    copyright.innerHTML = "&copy; " + date.getFullYear() + " Sellestiall Design. All Rights Reserved"
+    copyright.innerHTML = "&copy; " + date.getFullYear() + " Sellestiall Design. All Rights Reserved" + "<div class='mx-3'>/</div><a href='resume.html'>RESUME</a><div class='mx-3'>/</div><a href='index.html'>PROJECTS</a><div class='mx-3'>/</div><a href='resume.html#contact'>CONTACT</a><div class='mx-3'>/</div><a href='#'>BACK TO TOP</a>"
 }
 
 displayCopyright()
